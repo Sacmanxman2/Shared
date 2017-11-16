826e783f5f3eae355a7a7a6a7f8ae8b329928607
Sacmanxman2 <jake850@gmail.com>
2017-11-16 15:16:04 -0600

    Update TestNumba3.py

--- 2017-11-15/TestNumba3.py
+++ 2017-11-15/TestNumba3.py
@@ -3,13 +3,13 @@
 # 2017-11-15
 # Write a python program to cut out and display just the Preamble (We the People...). 
 
-with open("Constitution.txt") as f:
+with open("Constitution.txt") as f: #Open the file, read it into a string variable called USA
     USA = f.read()
 
-def StringSearch(string,term):
+def StringSearch(string,term): #Define a function to search for a word, which returns the number of words in something is
     Yes = False
     count = -1
-    for word in string.split():
+    for word in string.split(): #Iterate through each word in the string
         count +=1
         if word == term:
             Yes = True
@@ -20,4 +20,4 @@ def StringSearch(string,term):
 
 Start = StringSearch(USA,"We")
 End = StringSearch(USA,"America.") + 1
-print(" ".join(USA.split()[Start:End]))
+print(" ".join(USA.split()[Start:End])) # Prints out the words joined together with spaces
