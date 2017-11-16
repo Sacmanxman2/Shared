9069dd0637d8056486b26a7ae0ba0d89896e9012
Sacmanxman2 <jake850@gmail.com>
2017-11-16 15:14:02 -0600

    Delete TestNumba2.py

--- 2017-11-15/TestNumba2.py
+++
@@ -1,9 +0,0 @@
-# Test question 2
-# Jared Sackett
-# 2017-11-15
-# Write a python program to determine how many index values there are.
-
-with open(constitution.txt) as f:
-    USALines = f.readlines()
-    USA = f.read()
-
