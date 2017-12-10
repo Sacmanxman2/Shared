608a13209ba128743271c09223a70ed44f8b3ca6
Sacmanxman2 <jake850@gmail.com>
2017-12-09 23:56:12 -0600

    Update Final-Q9.py

--- Final-Q9.py
+++ Final-Q9.py
@@ -4,14 +4,18 @@
 
 fil = 'biglist.txt'
 
-with open(fil) as f:
-    text = f.readlines()
+# Again with my personal preference for opening files.
+#with open(fil) as f:
+#    text = f.readlines()
 
-def readXlines(x,text=text):
+f = open(fil)
+realtext = f.readlines()
+
+def readXlines(x,text=realtext): # define a function that takes X as how many lines, and text for what text to scan (I have it set by default to our other text variable)
     for line in text:
         print(line)
-        x-=1
+        x-=1 # This is a weird way to iterate through a for loop x amounts of times, but it works. A while loop would work too, but that would make iterating through the lines more annoying.
         if x<=0:
             break
 
-readXlines(12)
+readXlines(12) # We don't have to define text since the default works with our variable
