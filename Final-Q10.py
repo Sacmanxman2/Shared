a876eefa77599725f13ec2524cea81f4de5c25b6
Sacmanxman2 <jake850@gmail.com>
2017-12-09 23:46:16 -0600

    Update Final-Q10.py

--- Final-Q10.py
+++ Final-Q10.py
@@ -2,9 +2,14 @@
 # Question 10
 # write a python program to find all words that are twelve characters long in biglist.txt
 
-with open('biglist.txt') as f:
-    bigList = f.readlines()
-
+# This is how I like to open files, but our prof has taught us a different way.
+#with open('biglist.txt') as f: # The with statement closes the file when we're done with it, that's why I like it. Not a big deal at this time.
+#    bigList = f.readlines()
+    
+# The different way that you should use
+f = open('biglist.txt')
+bigList = f.readlines()
+    
 for line in bigList:
-    if len(line.rstrip()) == 12:
+    if len(line.rstrip()) == 12: #rstrip removes those annoying newline (\n) characters from the calculation. Otherwise we'd get 10 character words
         print(line)
